00f2e4941eedf696907d14966b4f022bbfb23784
AARON M <asmarcks@gmail.com>
2019-12-11 02:11:27 -0600

    scores updating and active player

--- app.js
+++ app.js
@@ -11,13 +11,18 @@ GAME RULES:
 
 var scores = [0,0];
 var roundScore = 0;
-var activePlayer = 1;
+var activePlayer = 0;
 // var dice = Math.floor(Math.random()*6) +1;
 // console.log(dice);
-var x = document.querySelector('#score-0').textContent;
-console.log(x);
+// var x = document.querySelector('#score-0').textContent;
+// console.log(x);
 // document.querySelector('#current-' + activePlayer).textContent = dice;
 // document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + "</em>"
+
+document.getElementById('score-0').textContent = '0'
+document.getElementById('score-1').textContent = '0'
+document.getElementById('current-0').textContent = '0'
+document.getElementById('current-1').textContent = '0'
 
 document.querySelector('.dice').style.display = 'none';
 
@@ -28,6 +33,20 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
     var diceDOM = document.querySelector('.dice')
     diceDOM.style.display = 'block';
     diceDOM.src = 'dice-' + dice + '.png';
-
+    
     //update the round score if the rolled number was not a 1
+    if (dice !== 1){
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+
+    }else {
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+    }
+
 });
