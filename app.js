ff9ed06dc2d65f10d55ccb9f4dac99c297f72fb4
AARON M <asmarcks@gmail.com>
2019-12-12 02:07:10 -0600

    added intitalize function for new game

--- app.js
+++ app.js
@@ -8,11 +8,9 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-var globalScore = 0;
-var scores = [0, 0];
-var roundScore = 0;
-var activePlayer = 0;
+var scores, roundScore, activePlayer;
 
+init();
 
 function nextPlayer() {
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
@@ -28,11 +26,6 @@ function nextPlayer() {
 
 
 
-document.getElementById('score-0').textContent = '0'
-document.getElementById('score-1').textContent = '0'
-document.getElementById('current-0').textContent = '0'
-document.getElementById('current-1').textContent = '0'
-document.querySelector('.dice').style.display = 'none';
 
 
 document.querySelector('.btn-roll').addEventListener('click', function () {
@@ -71,19 +64,29 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
     }
 });
 
-// document.querySelector('.btn-new').addEventListener('click', function () {
-
-//     document.getElementById('score-0').textContent = '0'
-//     document.getElementById('score-1').textContent = '0'
-//     document.getElementById('current-0').textContent = '0'
-//     document.getElementById('current-1').textContent = '0'
-//     document.querySelector('.dice').style.display = 'none';
-//     // document.querySelector('.player-' + activePlayer + '-panel').classList.toggle('active');
-//     // document.querySelector('.player-' + activePlayer + '-panel').classList.toggle('winner');
+document.querySelector('.btn-new').addEventListener('click',  init);
 
 
-// })
+function init() {
+    scores = [0, 0];
+    roundScore = 0;
+    activePlayer = 0;
 
+    document.getElementById('score-0').textContent = '0'
+    document.getElementById('score-1').textContent = '0'
+    document.getElementById('current-0').textContent = '0'
+    document.getElementById('current-1').textContent = '0'
+    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+
+
+}
 
 
 // var dice = Math.floor(Math.random()*6) +1;
